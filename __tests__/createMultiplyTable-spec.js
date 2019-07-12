d8fff2404850961f090bb4b3d510ec5f3628251a
Songbai Yan <colorguitar@hotmail.com>
2019-07-12 10:46:59 +0800

    create multiply table

--- __tests__/createMultiplyTable-spec.js
+++ __tests__/createMultiplyTable-spec.js
@@ -1,7 +1,23 @@
-// const add = require('../main');
+// import {
+//     checkStartNumberIsLessThanOrEqualsEndNumber,
+//     multiply,
+//     createMultiplyTable
+//   } from "../createMultiplyTable.js";
 
-it ('should add two numbers', () => {
-    expect(2+3).toBe(5);
+const { checkStartNumberIsLessThanOrEqualsEndNumber,  multiply } = require("../createMultiplyTable")
+// const multiply = require("../createMultiplyTable")
+
+it ('should check 2 is less than 3', () => {
+    expect(checkStartNumberIsLessThanOrEqualsEndNumber(2,3)).toBe(true);
 });
 
-// node_modules/.bin/jest
+it ('should get multiply string given 2 and 2', () => {
+    expect(multiply(2, 2)).toBe("2*2=4");
+});
+
+it ('should get multiply table given 2 and 3', () => {
+    expect(multiply(2, 3)).toBe("2*3=6");
+});
+
+
+// node_modules/.bin/jest   
--- createMultiplyTable.js
+++ createMultiplyTable.js
@@ -1,7 +1,30 @@
-function multiply(first, second) {
-    return first * second
+function multiply(start, end) {
+    return start + '*' + end + '=' + start * end
 }
 
-console.log(multiply(2, 3))
+function checkStartNumberIsLessThanOrEqualsEndNumber(start, end) {
+    return start <= end
+}
 
-// module.exports = add;
+module.exports = {
+    checkStartNumberIsLessThanOrEqualsEndNumber,
+    multiply,
+    createMultiplyTable
+}
+
+function createMultiplyTable(start, end) {
+    if (checkStartNumberIsLessThanOrEqualsEndNumber(start, end)) {
+        for (i = start; i <= end; i++) {
+            str = ''
+            for (j = start; j <= i; j++){
+                multiplyStr = multiply(j, i)
+                str += multiplyStr + ' '
+            }
+            console.log(str)
+        }
+    } else {
+        console.log('null')
+    }
+}
+
+createMultiplyTable(2, 4)
